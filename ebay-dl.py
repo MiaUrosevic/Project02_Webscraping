bee54b4cd99c1c651ace4e95db9c7839a2a9bfc4
Mia Urosevic <miaurosevic21@gmail.com>
2026-03-11 11:46:04 -0700

    project

--- ebay-dl.py
+++ ebay-dl.py
@@ -1,156 +1,53 @@
-import argparse
 import requests
 from bs4 import BeautifulSoup
-import json
-import csv
-import re
+import pprint
 
-
-# -----------------------------
-# Command line arguments
-# -----------------------------
-parser = argparse.ArgumentParser()
-parser.add_argument("search_term", help="item to search on ebay")
-parser.add_argument("--csv", action="store_true", help="save output as csv instead of json")
-
+import argparse
+parser = argparse.ArgumentParser(description='Download information from ebay and convert to JSON.')
+parser.add_argument('search_term')
+parser.add_argument('--num_pages', default=1)
 args = parser.parse_args()
+print('args.search_term=', args.search_term)
 
-search_term = args.search_term
-search_term_url = search_term.replace(" ", "+")
+for page_number in range(1, int(args.num_pages)+1):
 
+    # build the url
+    url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw='
+    url += args.search_term
+    url += '&_sacat=0&LH_TitleDesc=0&_pgn='
+    url += str(page_number)
+    url += '&rt=nc'
+    print('url=', url)
 
-# -----------------------------
-# Store results
-# -----------------------------
-items = []
+    # download the html
+    r = requests.get(url)
+    status = r.status_code
+    print('status=', status)
+    html = r.text
 
+    # process the html
+    items = []
+    soup = BeautifulSoup(html)
 
-# -----------------------------
-# Loop through first 10 pages
-# -----------------------------
-for page in range(1, 11):
+    tags_items = soup.select('.s-item')
+    item = {}
+    for tag in tags_items:
 
-    url = f"https://www.ebay.com/sch/i.html?_nkw={search_term_url}&_pgn={page}"
+        name = None
+        tags_name = tag.select('.s-item__title')
+        for tag in tags_name:
+            name = tag.text
 
-    response = requests.get(url)
+        freereturns = False
+        tags_freereturns = tag.select('.s-item__free-returns')
+        for tag in tags_freereturns:
+            freereturns = True
 
-    soup = BeautifulSoup(response.text, "html.parser")
+        items.append({
+            'name': name,
+            'freereturns': freereturns,
+            })
 
-    results = soup.find_all("li", class_="s-item")
-
-    for r in results:
-
-        # -----------------------------
-        # Name
-        # -----------------------------
-        name_tag = r.find("div", class_="s-item__title")
-        name = name_tag.text if name_tag else None
-
-
-        # -----------------------------
-        # Price (convert to cents)
-        # -----------------------------
-        price_tag = r.find("span", class_="s-item__price")
-        price = None
-
-        if price_tag:
-            text = price_tag.text
-            match = re.search(r"\$([\d\.]+)", text)
-
-            if match:
-                price = int(float(match.group(1)) * 100)
-
-
-        # -----------------------------
-        # Item condition
-        # -----------------------------
-        status_tag = r.find("span", class_="SECONDARY_INFO")
-        status = status_tag.text if status_tag else None
-
-
-        # -----------------------------
-        # Shipping cost
-        # -----------------------------
-        shipping_tag = r.find("span", class_="s-item__shipping")
-        shipping = None
-
-        if shipping_tag:
-            text = shipping_tag.text.lower()
-
-            if "free" in text:
-                shipping = 0
-            else:
-                match = re.search(r"\$([\d\.]+)", text)
-
-                if match:
-                    shipping = int(float(match.group(1)) * 100)
-
-
-        # -----------------------------
-        # Free returns
-        # -----------------------------
-        returns_tag = r.find("span", class_="s-item__free-returns")
-        free_returns = True if returns_tag else False
-
-
-        # -----------------------------
-        # Items sold
-        # -----------------------------
-        sold_tag = r.find("span", class_="s-item__hotness")
-        items_sold = None
-
-        if sold_tag:
-            match = re.search(r"\d+", sold_tag.text)
-
-            if match:
-                items_sold = int(match.group())
-
-
-        # -----------------------------
-        # Create dictionary
-        # -----------------------------
-        item = {
-            "name": name,
-            "price": price,
-            "status": status,
-            "shipping": shipping,
-            "free_returns": free_returns,
-            "items_sold": items_sold
-        }
-
-        items.append(item)
-
-
-# -----------------------------
-# Save output file
-# -----------------------------
-filename_base = search_term.replace(" ", "_")
-
-if args.csv:
-
-    filename = filename_base + ".csv"
-
-    with open(filename, "w", newline="") as f:
-        writer = csv.DictWriter(
-            f,
-            fieldnames=[
-                "name",
-                "price",
-                "status",
-                "shipping",
-                "free_returns",
-                "items_sold"
-            ]
-        )
-
-        writer.writeheader()
-
-        for item in items:
-            writer.writerow(item)
-
-else:
-
-    filename = filename_base + ".json"
-
-    with open(filename, "w") as f:
-        json.dump(items, f, indent=4)
+    #print('len(tags_name)=', len(tags_name))
+    #print('len(tags_freereturns)=', len(tags_freereturns))
+    pprint.pprint(items)
